a527994c2d99fb6af735098371c2733a848518c7
pablocavalcante <pablocavalcante@prodam.sp.gov.br>
2025-11-18 12:11:25 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,11 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Program {
@@ -13,8 +16,14 @@ public class Program {
 
         System.out.println("=== TESTE 1: Seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("\n=== TESTE 2: Seller findByDepartmend ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
 
     }
 }
