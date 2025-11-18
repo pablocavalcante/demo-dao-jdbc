472bd022f11dfcbe5b4f935eb5902bbadad3c799
pablocavalcante <pablocavalcante@prodam.sp.gov.br>
2025-11-18 10:13:57 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(10, "Pablo Cavalcante", "pablo@gmail.com", new Date(), 4500.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
         System.out.println(seller);
